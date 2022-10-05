a67d2d690b26a2010f5ce2c99e1b49bc6e4c49e0
Alexmachine1 <meklex1@gmail.com>
2022-10-05 19:01:54 +0100

    Reset btn added

--- script.js
+++ script.js
@@ -9,38 +9,55 @@ let count = 0
 
 function add1(){
     count += 1
-firstScore.textContent = count
-    console.log(count)
+    firstScore.textContent = count
+    
 }
 
 function add2(){
     count += 2
-firstScore.textContent = count
-    console.log(count)
+    firstScore.textContent = count
+    
 }
 
 function add3(){
     count += 3
-firstScore.textContent = count
-    console.log(count)
+    firstScore.textContent = count
+    
 }
 
 let count1 = 0
 // guestScore
 function addGuest1(){
     count1 += 1
-guestScore.textContent = count1
-    console.log(count)
+    guestScore.textContent = count1
+
 }
 
 function addGuest2(){
     count1 += 2
-guestScore.textContent = count1
-    console.log(count)
+    guestScore.textContent = count1
 }
 
 function addGuest3(){
     count1 += 3
-guestScore.textContent = count1
-    // console.log(count)
+    guestScore.textContent = count1
 }
+
+function reset(){
+    count = 0
+    count1 = 0
+    // console.log(count)
+    guestScore.textContent = count1
+    firstScore.textContent = count
+}
+
+// function winLose(){
+    if (count > count1){
+        console.log('Home is winning!')
+    }else if (count < count1) {
+        console.log('Guest is winning!')
+    }else {
+        console.log("It's a draw!")
+    }
+// }
+// winLose()
